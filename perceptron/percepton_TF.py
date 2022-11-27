40c083823637435bf82c87c1ead17a8b2b336ba6
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-11-27 10:26:08 +0100

    funciona

--- perceptron/percepton_TF.py
+++ perceptron/percepton_TF.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 
 class percepton():
     def __init__ (self, obs_entr, prediccion):
-        self.obs_entr = obs_entr
-        self.prediccion = prediccion
+        self.valores_entradas_X = obs_entr
+        self.valores_a_predecir_Y = prediccion
         self.tf_neuronas_entradas_X = tf.placeholder(tf.float32, [None, 2])
         self.tf_valores_reales_Y = tf.placeholder(tf.float32, [None, 1])
         self.peso = tf.Variable(tf.random_normal([2, 1]), tf.float32)
@@ -17,14 +17,14 @@ class percepton():
         sumaponderada = tf.add(sumaponderada,self.sesgo)
         prediccion = tf.sigmoid(sumaponderada)
         funcion_error = tf.reduce_sum(tf.pow(self.tf_valores_reales_Y-prediccion,2))
-        return funcion_error
+        return prediccion, funcion_error
 
 
-    def optimizar(self):
-        optimizador = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(self.funcion_error)
+    def optimizar(self, funcion_error):
+        optimizador = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(funcion_error)
         return optimizador
 
-    def aprendizaje(self, epochs, funcion_error, optimizador):
+    def aprendizaje(self, epochs, funcion_error, optimizador, prediccion):
         init = tf.global_variables_initializer()
         #Inicio de una sesión de aprendizaje
         sesion = tf.Session()
@@ -45,27 +45,26 @@ class percepton():
             Grafica_MSE.append(MSE)
             print("EPOCH (" + str(i) + "/" + str(epochs) + ") -  MSE: "+ str(MSE))
 
-            return Grafica_MSE
+
+        print("--- VERIFICACIONES ----")
+        for i in range(0,4):
+            print("Observación:"+str(self.valores_entradas_X[i])+ " - Esperado: "+str(self.valores_a_predecir_Y[i])+" - Predicción: "+str(sesion.run(prediccion, feed_dict={self.tf_neuronas_entradas_X: [self.valores_entradas_X[i]]})))
+
+        return Grafica_MSE
 
     def plot(self, grafica):
         plt.plot(grafica)
         plt.ylabel('MSE')
         plt.show()
 
-    def verificacion(self):
-        print("--- VERIFICACIONES ----")
-        for i in range(0,4):
-            print("Observación:"+str(self.valores_entradas_X[i])+ " - Esperado: "+str(self.valores_a_predecir_Y[i])+" - Predicción: "+str(sesion.run(self.prediccion, feed_dict={self.tf_neuronas_entradas_X: [self.valores_entradas_X[i]]})))
-
 
 
 def main():
     percept = percepton([[1., 0.], [1., 1.], [0., 1.], [0., 0.]],  [[0.], [1.], [0.], [0.]])
-    funcion_error = percept.sumaponderada()
-    optimizador = percept.optimizar()
-    graf = percept.aprendizaje(3000, funcion_error, optimizador)
-    percept.plot()
-    percept.verificacion()
+    prediccion, funcion_error = percept.sumaponderada()
+    optimizador = percept.optimizar(funcion_error)
+    graf = percept.aprendizaje(3000, funcion_error, optimizador, prediccion)
+    percept.plot(graf)
 
 
 if __name__ == "__main__":
